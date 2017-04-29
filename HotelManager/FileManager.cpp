d7d85d30f02d24b87649f52dc732c1d2b10fe454
stranger219 <mike.petrou9558@gmail.com>
2017-04-29 20:44:52 +0300

    Adding some comments

--- HotelManager/FileManager.cpp
+++ HotelManager/FileManager.cpp
@@ -12,18 +12,22 @@ using namespace std;
 void readFile()
 {
 	long int numberOfHotels;
+
 	ifstream readFile("modData.csv");
+
 	string line;
 	string value;
 	//char *buffer = nullptr;
 
-	//Count number of records on the first line of the file
+	//Count number of records on the first line of the file and convert it to integer
 	getline(readFile, value, ';');
 
 	numberOfHotels = stoi(value);
 	
-
-	cout <<"Number of hotels: " << numberOfHotels <<endl;
+	//testing if it gets the correct number from the file
+	//cout <<"Number of hotels: " << numberOfHotels <<endl;
+	
+	//reading each line
 	while (readFile.good())
 	{
 		
--- HotelManager/Main.cpp
+++ HotelManager/Main.cpp
@@ -15,6 +15,7 @@ int main()
 	return 0;
 }
 
+//creating the menu
  void printMenu()
 {
 	 cout << "   ------------------------------------------  " << endl;
